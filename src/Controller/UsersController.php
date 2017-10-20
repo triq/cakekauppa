003ff29e35d1a0f73299bd8bccca7621dc9b9dba
triq <triq001@gmail.com>
2017-10-20 21:01:31 +0300

    Login function.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,6 +13,11 @@ use App\Controller\AppController;
 class UsersController extends AppController
 {
 
+    public function initialize() {
+        parent::initialize();
+        $loginRedirect = '/products/';
+        $this->Auth->config('loginRedirect', $loginRedirect);
+    }
     /**
      * Index method
      *
@@ -107,5 +112,23 @@ class UsersController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+    public function login() {
+        if($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+
+            //User not identified
+            $this->Flash->error('Your username or password is incorrect');
+        }
+    }
+
+    public function logout() {
+        $this->Flash->success("You are now logged out.");
+        return $this->redirect($this->Auth->logout());
     }
 }
